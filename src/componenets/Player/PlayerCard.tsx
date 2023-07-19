8042acd7e853cd7eb6c6a8a2684a14bdc359c4e3
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-19 17:03:45 +0300

    feat: rewrote PlayerCard using context + xstate machine

---
+++ src/componenets/Player/PlayerCard.tsx
@@ -0,0 +1,26 @@
+import { HandProvider } from './contexts/HandContext';
+import usePlayer from './hooks/usePlayer';
+import HandCard from './HandCard/HandCard';
+import { type Player } from '~/lib/machines/gameMachine';
+
+export default function PlayerCard() {
+  const { player } = usePlayer();
+
+  return (
+    <div className='player border border-gray-500 rounded p-4 shadow-lg flex flex-col items-center gap-2 w-1/3'>
+      <PlayerHeader player={player} />
+      {player.hands.map(hand => (
+        <HandProvider hand={hand} key={hand.id}>
+          <HandCard />
+        </HandProvider>
+      ))}
+    </div>
+  );
+}
+function PlayerHeader({ player }: { player: Player }) {
+  return (
+    <h2 className='text-xl font-bold'>
+      Player {player.id} | {player.strategy} | Balance: {player.balance}
+    </h2>
+  );
+}
