0b40b8b0acfcd43eddd260043df3de6b2a9a469f
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-19 16:44:08 +0300

    added isHandBlackjack help funciton

---
+++ src/lib/machines/utils.ts
@@ -0,0 +1,8 @@
+import { calculateHand } from '../calculateHand';
+import { Card } from '../deck';
+
+export function isHandBlackjack(cards: Card[]) {
+  if (cards.length !== 2) return false;
+  const { validCounts } = calculateHand(cards);
+  return validCounts[0] === 21;
+}
