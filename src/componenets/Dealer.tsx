4951ed118f830432cec499bc70b7252d211295e7
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-19 16:44:50 +0300

    feat: fixed Dealer components, using useDealerCount hook

--- src/componenets/Dealer.tsx
+++ src/componenets/Dealer.tsx
@@ -1,7 +1,6 @@
 import Card from './Card';
-import { calculateHand } from '../lib/calculateHand';
 import { useGameMachine } from '~/lib/machines/gameMachineContext';
-import { useMemo } from 'react';
+import { useDealerCount } from '../lib/hooks/useDealerCount';
 
 function DealerHand() {
   const { isRoundFinished } = useGameMachine();
@@ -19,39 +18,18 @@ function DealerHand() {
     </div>
   );
 }
-function useDealerCount() {
-  const { state } = useGameMachine();
-  const dealer = state.context.dealer;
-  const allPlayersFinishedRound = useMemo(
-    () => state.context.players.every(player => player.hands.every(hand => hand.isFinished)),
-    [state.context.players],
-  );
-  const { validCounts, bustCount } = useMemo(() => {
-    const cards = allPlayersFinishedRound ? dealer.hand.cards : dealer.hand.cards.slice(1);
-    return calculateHand(cards);
-  }, [dealer.hand.cards, allPlayersFinishedRound]);
-  const visibleDealerCount = validCounts.length > 0 ? validCounts : bustCount;
-  const didBust = validCounts.length === 0 && bustCount > 21;
-  return {
-    validCounts,
-    bustCount,
-    visibleDealerCount,
-    didBust,
-  };
-}
-
 function DealerCount() {
-  const { visibleDealerCount, didBust } = useDealerCount();
-
+  const { visible: visibleCount } = useDealerCount();
+  visibleCount.validCounts;
   return (
-    visibleDealerCount && (
+    visibleCount.finalCount && (
       <h3 className='text-lg ml-2 font-bold'>
         Count:{` `}
-        <span className={`text-xl font-bold ${didBust ? 'text-red-700' : 'text-green-600'}`}>
-          {Array.isArray(visibleDealerCount) && visibleDealerCount.length > 0
-            ? visibleDealerCount.join(' | ')
-            : visibleDealerCount}
-        </span>
+        {visibleCount.didBust ? (
+          <span className={'text-xl font-bold text-red-700'}>{visibleCount.bustCount}</span>
+        ) : (
+          <span className={'text-xl font-bold text-green-600'}> {visibleCount.validCounts.join(' | ')}</span>
+        )}
       </h3>
     )
   );
@@ -68,10 +46,10 @@ function DealerCards() {
   );
 }
 function EndGameDealerMessage() {
-  const dealerHasBlackjack = false; // todo! implement
-  const { visibleDealerCount } = useDealerCount();
-  const finalCount = typeof visibleDealerCount === 'number' ? visibleDealerCount : visibleDealerCount[0]!;
-  const dealerOutcomeMessage = finalCount > 21 ? 'Bust' : dealerHasBlackjack ? 'Blackjack!' : '';
+  const {
+    includeNonVisible: { didBust, isBlackjack },
+  } = useDealerCount();
+  const dealerOutcomeMessage = didBust ? 'Bust' : isBlackjack ? 'Blackjack!' : '';
 
   return (
     <>
