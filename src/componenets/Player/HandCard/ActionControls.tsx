0592d9b8593d9964e1612cc273900197038f2d3c
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-21 00:39:32 +0300

    feat: added automation for taking play-action for bots, and interactive player. refacetored ActionsControls code.

--- src/componenets/Player/HandCard/ActionControls.tsx
+++ src/componenets/Player/HandCard/ActionControls.tsx
@@ -3,51 +3,62 @@ import { useSettingsStore } from '~/stores/settingsStore';
 import { useGameMachine } from '~/lib/machines/gameMachineContext';
 import { useDealerCount } from '~/lib/hooks/useDealerCount';
 import usePlayerHand from '../hooks/usePlayerHand';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 export default function ActionControls() {
   const { send } = useGameMachine();
-  const { hand, player, isCurrentTurnHand, currentTurnInfo } = usePlayerHand();
+  const { hand, isCurrentTurnHand, player } = usePlayerHand();
 
-  const hit = () => isCurrentTurnHand && send({ type: 'HIT' });
-  const double = () => isCurrentTurnHand && send({ type: 'DOUBLE' });
-  const stand = () => isCurrentTurnHand && send({ type: 'STAND' });
-  const split = () => isCurrentTurnHand && send({ type: 'SPLIT' });
+  const hit = useCallback(() => isCurrentTurnHand && send({ type: 'HIT' }), [isCurrentTurnHand, send]);
+  const double = useCallback(() => isCurrentTurnHand && send({ type: 'DOUBLE' }), [isCurrentTurnHand, send]);
+  const stand = useCallback(() => isCurrentTurnHand && send({ type: 'STAND' }), [isCurrentTurnHand, send]);
+  const split = useCallback(() => isCurrentTurnHand && send({ type: 'SPLIT' }), [isCurrentTurnHand, send]);
 
   const { visible: visibleDealerCount } = useDealerCount();
-  const { allowedToDoubleAfterSplit } = useSettingsStore();
+  const { allowedToDoubleAfterSplit, automateInteractivePlayer } = useSettingsStore();
   const canDouble = hand.cards.length === 2;
   const canSplit = hand.cards.length === 2 && hand.cards[0]!.value === hand.cards[1]!.value;
 
-  const recommendedAction =
-    getActionByStrategy(hand.cards, visibleDealerCount.validCounts[0]!, {
-      allowedToDoubleAfterSplit,
-      canDouble,
-    }) || undefined;
-
+  const recommendedAction = useMemo(
+    () =>
+      getActionByStrategy(hand.cards, visibleDealerCount.validCounts[0]!, {
+        allowedToDoubleAfterSplit,
+        canDouble,
+      }) || undefined,
+    [allowedToDoubleAfterSplit, canDouble, hand.cards, visibleDealerCount.validCounts],
+  );
   const strategy = {
     recommendation: recommendedAction,
     styles: 'border-4 border-orange-300 rounded',
   };
-  // const alreadyClicked = useRef(false);
-  // useEffect(() => {
-  //   // if (!isCurrentTurn || alreadyClicked.current) return;
-  //   if (!isCurrentTurn) return;
 
-  //   switch (recommendedAction) {
-  //     case 'H':
-  //       hit(player.id);
-  //       break;
-  //     case 'S':
-  //       stand(player.id);
-  //       break;
-  //     case 'D':
-  //       double(player.id);
-  //       break;
-  //     default:
-  //       throw new Error('Invalid action');
-  //   }
-  //   // alreadyClicked.current = true;
-  // }, [double, hit, isCurrentTurn, player.id, recommendedAction, stand]);
+  /* AutomatePlayerActionForBots */
+  useEffect(() => {
+    if (!isCurrentTurnHand || (player.strategy === 'interactive' && !automateInteractivePlayer)) return;
+    const actionMap = {
+      H: hit,
+      S: stand,
+      D: double,
+      SP: split,
+    } as const;
+    const sendAction = actionMap[strategy.recommendation];
+    setTimeout(() => {
+      console.log(`player: ${player.id}: sendAction`, strategy.recommendation);
+
+      sendAction();
+    }, 300);
+  }, [
+    double,
+    hit,
+    isCurrentTurnHand,
+    split,
+    stand,
+    strategy.recommendation,
+    hand.cards.length,
+    player.strategy,
+    automateInteractivePlayer,
+    player.id,
+  ]);
 
   return (
     <div className='actions flex justify-center gap-4'>
