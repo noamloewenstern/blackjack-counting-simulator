1d202cbd3f92398755b40525118163d1243df8e2
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-23 22:04:21 +0300

    add: more tests to blackjack-calculations

--- src/lib/strategies/perfect-blackjack.test.ts
+++ src/lib/strategies/perfect-blackjack.test.ts
@@ -1,8 +1,9 @@
-import { ActionSettings, getActionByStrategy, isSoftHand } from './perfect-blackjack';
+import { ActionSettings, getActionByStrategy, isSoftHand, getSplitAction } from './perfect-blackjack';
 
-const settings: ActionSettings = {
-  canDouble: true,
+const settings = {
+  allowedToDouble: true,
   allowedToDoubleAfterSplit: true,
+  canDoubleAfterSplit: true,
 };
 describe('Perfect Blackjack', () => {
   describe('isSoftHand', () => {
@@ -16,12 +17,36 @@ describe('Perfect Blackjack', () => {
     });
   });
 
+  describe('split', () => {
+    describe("agains dealer's 10", () => {
+      const dealerCount = 10;
+      it('shoud return Y', () => {
+        const action = getSplitAction(['8', '8'], dealerCount, settings);
+        expect(action).toEqual('Y');
+      });
+    });
+  });
   describe('getActionByStrategy', () => {
     describe("agains dealer's 10", () => {
       const dealerCount = 10;
-      it('9,T should return S', () => {
+      it('Should Stand', () => {
         const action = getActionByStrategy(['9', 'T'], dealerCount, settings);
         expect(action).toEqual('S');
+      });
+      it('Should Hit', () => {
+        const action = getActionByStrategy(['Q', '4'], dealerCount, settings);
+        expect(action).toEqual('H');
+      });
+      it('Should Hit', () => {
+        const action = getActionByStrategy(['4', '9'], dealerCount, settings);
+        expect(action).toEqual('H');
+      });
+    });
+    describe("agains dealer's 7", () => {
+      const dealerCount = 7;
+      it('Should Stand', () => {
+        const action = getActionByStrategy(['5', 'K'], dealerCount, settings);
+        expect(action).toEqual('H');
       });
     });
   });
