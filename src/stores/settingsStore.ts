6366058ebf6ebf058f93ce7b2beb0fd340be5ca4
Noam Loewenstern <noamloewenstern@gmail.com>
2023-07-23 14:34:39 +0300

    feat: settings for automateInteractivePlayer

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -1,15 +1,22 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
-
+type AutomatePlay = {
+  isOn: boolean;
+  toggle: () => void;
+  intervalWaitBetweenPlays: number;
+  setIntervalWaitBetweenPlays: (interval: number) => void;
+};
 type DeckStore = {
   numberDecksInShoe: number;
   dealerMustHitOnSoft17: boolean;
   allowedToDoubleAfterSplit: boolean;
 
   automateInteractivePlayer: boolean;
+  autoPlay: AutomatePlay;
 };
 type Actions = {
   setNumberOfDecks: (numberOfDecks: number) => void;
+  toggleAutomateInteractivePlayer: () => void;
 };
 
 export const useSettingsStore = create(
@@ -17,8 +24,19 @@ export const useSettingsStore = create(
     numberDecksInShoe: 4,
     dealerMustHitOnSoft17: true,
     allowedToDoubleAfterSplit: true,
-    setNumberOfDecks: (numberOfDecks: number) => set({ numberDecksInShoe: numberOfDecks }),
 
-    automateInteractivePlayer: true,
+    // automateInteractivePlayer: true,
+    automateInteractivePlayer: false,
+    autoPlay: {
+      isOn: false,
+      toggle: () => set(state => ({ autoPlay: { ...state.autoPlay, automatePlay: !state.autoPlay.isOn } })),
+      intervalWaitBetweenPlays: 1000,
+      setIntervalWaitBetweenPlays: (interval: number) =>
+        set(state => ({ autoPlay: { ...state.autoPlay, intervalWaitBetweenPlays: interval } })),
+    },
+
+    setNumberOfDecks: (numberOfDecks: number) => set({ numberDecksInShoe: numberOfDecks }),
+    toggleAutomateInteractivePlayer: () =>
+      set(state => ({ automateInteractivePlayer: !state.automateInteractivePlayer })),
   })),
 );
